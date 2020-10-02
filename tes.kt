6ebef25b8b0a611f9e3c9cae580b771cf037e13e
phpimamikh <52100594+phpimamikh@users.noreply.github.com>
2020-10-02 09:07:45 +0700

    Create tes.kt

---
+++ tes.kt
@@ -0,0 +1,20 @@
+public class Customer {
+
+    private String name;
+
+    public Customer(String s){
+        name = s;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void placeOrder() {
+        System.out.println("jenengmu ki " + name);
+    }
+}
